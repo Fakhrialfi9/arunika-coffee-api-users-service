b6233f6223f8ed8567e68d58a3f8935d6b194ebf
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-17 17:28:54 +0700

    fix(step-07): format users module

--- src/application/users/users.module.ts
+++ src/application/users/users.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import {
-  USER_REPOSITORY,
-} from '../../domain/users/repositories/user.repository.js';
+import { USER_REPOSITORY } from '../../domain/users/repositories/user.repository.js';
 import { PrismaAuthenticationUserRepository } from '../../infrastructure/database/repositories/prisma-authentication-user.repository.js';
 import { CreateUserService } from './services/create-user.service.js';
 
