f449aa5a26a69d636237d0e5c51726851654c5b4
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-18 19:07:38 +0700

    feat(users): register gRPC controller

--- src/application/users/users.module.ts
+++ src/application/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { USER_REPOSITORY } from '../../domain/users/repositories/user.repository.js';
 import { PrismaAuthenticationUserRepository } from '../../infrastructure/database/repositories/prisma-authentication-user.repository.js';
+import { UsersGrpcController } from '../../presentation/grpc/users-grpc.controller.js';
 import { CreateUserService } from './services/create-user.service.js';
 import { DeleteUserService } from './services/delete-user.service.js';
 import { GetUserService } from './services/get-user.service.js';
@@ -9,7 +10,9 @@ import { ListUsersService } from './services/list-users.service.js';
 import { UpdateUserService } from './services/update-user.service.js';
 
 @Module({
+  controllers: [UsersGrpcController],
   providers: [
+    PrismaAuthenticationUserRepository,
     {
       provide: USER_REPOSITORY,
       useExisting: PrismaAuthenticationUserRepository,
