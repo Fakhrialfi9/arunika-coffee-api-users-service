2c31b5b1868198ff2cb0ef9648c097480de73931
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-18 15:18:43 +0700

    feat: add repository error model

---
+++ src/infrastructure/database/errors/repository.error.ts
@@ -0,0 +1,34 @@
+export abstract class RepositoryError extends Error {
+  abstract readonly code: string;
+
+  protected constructor(message: string, options?: { cause?: unknown }) {
+    super(message, options);
+    this.name = this.constructor.name;
+  }
+}
+
+export type UserUniqueField = 'username' | 'email' | 'phone';
+
+export class RepositoryUniqueConstraintError extends RepositoryError {
+  readonly code = 'REPOSITORY_UNIQUE_CONSTRAINT';
+
+  constructor(public readonly field: UserUniqueField, cause?: unknown) {
+    super(`Unique constraint violated for ${field}`, { cause });
+  }
+}
+
+export class RepositoryNotFoundError extends RepositoryError {
+  readonly code = 'REPOSITORY_NOT_FOUND';
+
+  constructor(cause?: unknown) {
+    super('Repository entity was not found', { cause });
+  }
+}
+
+export class RepositoryPersistenceError extends RepositoryError {
+  readonly code = 'REPOSITORY_PERSISTENCE_FAILURE';
+
+  constructor(cause?: unknown) {
+    super('Repository persistence operation failed', { cause });
+  }
+}
