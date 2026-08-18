1c8a800f4940c40e81738c890380a4e3a9c22fcc
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-18 19:07:53 +0700

    feat(users): start NestJS gRPC transport

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
 
+import { join } from 'node:path';
+
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 import type { AppConfig } from './config/app.config.js';
 import { AppModule } from './app.module.js';
@@ -13,9 +16,35 @@ async function bootstrap(): Promise<void> {
 
   const host = config.getOrThrow<AppConfig['host']>('app.host');
   const port = config.getOrThrow<AppConfig['port']>('app.port');
+  const grpcUsersHost = config.getOrThrow<AppConfig['grpcUsersHost']>(
+    'app.grpcUsersHost',
+  );
+  const grpcUsersPort = config.getOrThrow<AppConfig['grpcUsersPort']>(
+    'app.grpcUsersPort',
+  );
+
+  app.connectMicroservice({
+    transport: Transport.GRPC,
+    options: {
+      package: 'arunika.coffee.users.v1',
+      protoPath: join(
+        process.cwd(),
+        'proto/users/v1/users.proto',
+      ),
+      url: `${grpcUsersHost}:${grpcUsersPort}`,
+      loader: {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+      },
+    },
+  });
 
   app.enableShutdownHooks();
 
+  await app.startAllMicroservices();
   await app.listen(port, host);
 }
 
