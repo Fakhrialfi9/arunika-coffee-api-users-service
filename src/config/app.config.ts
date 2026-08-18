732a36d69fd1b2e3a05511428cda866f1cb632d1
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-18 19:07:01 +0700

    feat(users): add gRPC server configuration

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,6 +5,8 @@ export interface AppConfig {
   environment: string;
   host: string;
   port: number;
+  grpcUsersHost: string;
+  grpcUsersPort: number;
 }
 
 export const appConfig = registerAs('app', (): AppConfig => ({
@@ -12,4 +14,6 @@ export const appConfig = registerAs('app', (): AppConfig => ({
   environment: process.env.NODE_ENV ?? 'development',
   host: process.env.APP_HOST ?? '0.0.0.0',
   port: Number(process.env.APP_PORT ?? 3000),
+  grpcUsersHost: process.env.GRPC_USERS_HOST ?? '0.0.0.0',
+  grpcUsersPort: Number(process.env.GRPC_USERS_PORT ?? 50051),
 }));
